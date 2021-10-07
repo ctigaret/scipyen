3002ec905dc98a21653811a572c4097a8fb966de
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2021-10-07 21:55:18 +0100

    Restructuring of installation docs.
    scipyen/make_scripts_links.sh now makes a symbolic link to pyenv in $HOME/bin
    
    
    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- make_script_links.sh
+++ make_script_links.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-if [ -z ${VIRTUAL_ENV} ] ; then
-    echo "You must run this script while in a virtual python environment"
-fi
-
 realscript=`realpath $0`
 scipyendir=`dirname $realscript`
+
+if ! [ -r ${HOME}/bin/pyenv ] ; then
+    ln -s $scipyendir/doc/install/Python/Unix/pyenv ${HOME}/bin
+fi
+    
+echo "Initial pyenv script was linked to $HOME/bin/pyenv"
+echo "You must now make an alias in your .bashrc ."
+echo "See $scipyendir/docs/installation/Python/Unix/README for details"
+
+if [ -z ${VIRTUAL_ENV} ] ; then
+    
+    echo "You must run this script while in a virtual python environment"
+    echo "in order to install other scripts"
+    exit 
+fi
 
 if [ -z $BROWSER ]; then
     if [ -a $VIRTUAL_ENV/bin/browser ]; then
@@ -23,7 +34,7 @@ if [ -d $scipyendir/nrnipython ] ; then
     ln -s $scipyendir/nrnipython/nrnipython ${VIRTUAL_ENV}/bin/nrnpython
     ln -s $scipyendir/nrnipython/nrnipython ${VIRTUAL_ENV}/bin/nrnpy
 fi
-    
+
 
 
 
