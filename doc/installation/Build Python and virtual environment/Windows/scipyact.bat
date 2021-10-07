386b4560d8c8f30652cb04d132b25131a4a9778f
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2021-10-07 16:10:58 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/installation/Build Python and virtual environment/Windows/scipyact.bat
+++ doc/installation/Build Python and virtual environment/Windows/scipyact.bat
@@ -1,4 +1,7 @@
 @echo off
 rem  EDIT THE LOCATION OF THE scipyenv VIRTUAL ENVIRONMENT, BELOW (LEAVE Scripts\activate AS IS)
-"F:\Users\Public\scipyenv\Scripts\activate" 
+CALL "F:\Users\Public\scipyenv\Scripts\activate" 
+set LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%LIB%
+set LIBPATH=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%LIBPATH%
+set PATH=%VIRTUAL_ENV%\bin;%PATH%
 echo on
--- doc/installation/Build Python and virtual environment/Windows/scipyact_vs64.bat
+++ doc/installation/Build Python and virtual environment/Windows/scipyact_vs64.bat
@@ -5,9 +5,13 @@ rem  EDIT THE LOCATION OF THE vcvarsall.bat SCRIPT, BELOW
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 rem  SET "PROMPT=(x64) $P$G"
 rem  EDIT THE LOCATION OF THE scipyenv VIRTUAL ENVIRONMENT, BELOW (LEAVE Scripts\activate AS IS)
-"F:\Users\Public\scipyenv\Scripts\activate" 
+CALL "F:\Users\Public\scipyenv\Scripts\activate" 
 rem  Use the line below if scipyenv environment was created with msys2/mingw64
 REM echo "NOTE: For access to the MinGW-w64 toolchain call 'scipyact' from MinGW-w64 shell"
 REM "F:\Users\Public\scipyenv-mingw64\bin\activate"
+set INCLUDE=%VIRTUAL_ENV%\include;%INCLUDE%
+set LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%LIB%
+set LIBPATH=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%LIBPATH%
+set PATH=%VIRTUAL_ENV%\bin;%PATH%
 SET "PROMPT=(x64 scipyenv) $P$G"
 echo on
