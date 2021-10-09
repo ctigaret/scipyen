e010d4436d61b2f9b2125e214e1fb40ce13fbac0
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2021-10-09 15:54:43 +0100

    Updates windows scripts
    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/Python/Windows/scipyact.bat
+++ doc/install/Python/Windows/scipyact.bat
@@ -2,9 +2,9 @@
 REM Set up \Microsoft Visual Studio 2015, where <arch> is amd64, x86, etc.
 rem  CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 rem  SET "PROMPT=(x64) $P$G"
-REM EDIT BELW TO SET WHERE scipyenv HAS BEEN CREATED
+REM EDIT BELOW TO SET WHERE scipyenv HAS BEEN CREATED
 CALL "e:\scipyenv\Scripts\activate"
-set "LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%LIB%"
+set "LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%VIRTUAL_ENV%\lib\site-packages\vigra;%LIB%"
 set "LIBPATH=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib\site-packages\vigra;%VIRTUAL_ENV%\lib64;%LIBPATH%"
 set "INCLUDE=%VIRTUAL_ENV%\include;%INCLUDE%"
 set "PATH=%VIRTUAL_ENV%\bin;%VIRTUAL_ENV%\Scripts;%USERPROFILE%\AppData\Local\Programs\Python\Python39\DLLs;%PATH%"
