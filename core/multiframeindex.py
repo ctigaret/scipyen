24e173f47e78fe91bad31192387fae54e08567c3
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2021-12-01 23:21:45 +0000

    MultiFrameIndex and FrameIndexLookup seem to be working well
    NEXT: include these in the ScanData and AnalysisUnit API
    Can these be also used with PlanarGraphics?
    
    
    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- core/multiframeindex.py
+++ core/multiframeindex.py
@@ -1,13 +1,11 @@
 import os, typing, types
-from functools import partial
+from functools import partial, partialmethod
 from collections import namedtuple
 import collections.abc
 from inspect import (getmembers, getattr_static)
-from core.prog import (ArgumentError, OneOf, TypeValidator, GenericValidator,
-                       get_descriptors, get_properties,
-                       parse_descriptor_specification,
-                       WithDescriptors, setup_descriptor,
-                       classify_signature)
+from core.prog import (ArgumentError,  WithDescriptors, 
+                       get_descriptors, classify_signature)
+from core.basescipyen import BaseScipyenData
 
 __module_path__ = os.path.abspath(os.path.dirname(__file__))
 __module_name__ = os.path.splitext(os.path.basename(__file__))[0]
@@ -18,22 +16,7 @@ class MultiFrameIndex(WithDescriptors):
     """Maps a virtual data frame index to frame indices into its compoents
     """
         
-    #@classmethod
-    #def setup_descriptor(cls, descr_params):
-        #args = descr_params.get("args", tuple())
-        #kwargs = descr_params.get("kwargs", {})
-        #name = descr_params.get("name", "")
-        ##print(f"setting up {name} with {descr_params}")
-        #if not isinstance(name, str) or len(name.strip()) == 0:
-            #return
-        #descriptor = GenericValidator(*args, **kwargs)
-        #descriptor.allow_none = True
-        #descriptor.__set_name__(cls, name)
-        ##print(descriptor.private_name)
-        #setattr(cls, name, descriptor)
-        
     def __init__(self, *fields, **field_map):
-        #super(WithDescriptors, self).__init__()
         for field in fields:
             if not isinstance(field, str) or len(field.strip()) == 0:
                 raise TypeError(f"A field must be a non-empty string; got {field} instead")
@@ -48,7 +31,6 @@ class MultiFrameIndex(WithDescriptors):
                 raise TypeError(f"A field must be a non-empty string; got {field} instead")
             
             type(self).setup_descriptor({"name": field, "args": (int,),  "kwargs":{}})
-            #type(self).setup_descriptor({"name": field, "args": ((int, collections.abc.Sequence), int), "kwargs":{}})
             setattr(self, field, value)
               
     
@@ -93,9 +75,42 @@ class MultiFrameIndex(WithDescriptors):
 class FrameIndexLookup(object):
     """Tools for frame synchronization between scans, scene and ephys data.
     
-    FrameIndexLookup maps a virtual data frame to a frame index into each of its
-    data child components (for ScanData, these are: 'scene', 'scans' and 
-    'electrophysiology').
+    FrameIndexLookup is used with objects containing distinct data sets organized 
+    in frames (views into data subsets to be displayed and/or analysed separately
+    from the rest of the data). 
+    
+    The primary example are ScanData objects which contain a set of images 
+    ("scans"), and possibly a set of reference image data ("scene") and a set of
+    electrophysiology signals.
+    
+    Each of these data types can be viewed and analysed frame-by-frame, where a
+    'frame' associates a single image in "scans", with an image in the "scene",
+    and a corresponding 'sweep' in the electrophysiology data set.
+    
+    Ideally, there should be a direct, one-to-one correspondence between a frame
+    in all three data components ("scans", "scene" and "electrophysiology"). 
+    In practice this may not always happen e.g., when there is only one reference
+    image in "scene" for all images in "scans", or when the number of 
+    electrophysiology sweeps is different from the number of frames in the 
+    "scans" data.
+    
+    The FrameIndexLookup introduces the notion of "virtual data frame" where 
+    a data "frame index" is mapped to a real frame index into each of the 
+    data components (e.g., for the ScanData in the example above, a virtual
+    frame index would correspond to the frame with same index in "scans", but 
+    always to frame index 0 in "scene").
+    
+    The data components corresponding to the virtual frame 0 can be accessed via
+    FrameLookupIndex in a simple manner:
+    
+    frame[index].scene -> 0
+    frame[index].scans -> index
+    frame[index].electrophysiology -> None (assuming there is no electrophysiology
+    data available)
+    
+    where 'index' is any valid index (in this example, "scans" have the largest
+    number of frames and "drive" the frame indexing for the entire data in the 
+    ScanData object)
     
     The 'fields' in a FrameIndexLookup object are deduced from the data, or from
     the MultiFrameIndex objects passed in the constructor (see below).
@@ -120,82 +135,77 @@ class FrameIndexLookup(object):
         
     """
     
-    def __get_frame_index__(self, index:int, field: str):
-        """Returns master index if field not found or index not mapped
-        """
-        if index in self._map_:
-            return getattr(self._map_[index], field, index)
+    class __IndexProxy__(object):
+        def __init__(self, field:str):
+            if not isinstance(field, str) or len(field.strip()) == 0:
+                raise ArgumentError(f"Expecting a non-empty str; got {field} instead")
+            self._obj_ = None
+            self._field_ = field
+            
+        def __get__(self, obj, objtype=None):
+            # NOTE: 2021-12-01 22:44:11 reference needed in __get/setitem__
+            self._obj_ = obj 
+            # NOTE: 2021-12-01 22:08:48 
+            # returns self so that setitem and getitem can be called upon it
+            return self
         
-        return index
-    
-    def __set_frame_index__(self, index:int, field:str, value:typing.Union[int]=None):
-        """Set the data frame index for the master frame index, to value
-        """
-        if not isinstance(value, type(None)):
-            raise TypeError(f"Expecting an int or None; got {type(value).__name__} instead")
+        def __set__(self, obj, value=None):
+            # NOTE: 2021-12-01 22:44:11 reference needed in __get/setitem__
+            self._obj_ = obj
         
-        if index in self._map_:
-            if field in self._map_[index]:
-                setattr(self._map_[index], field, value)
-            else: # CAUTION: subclasses of MultiFrameIndex MAY be immutable
-                try:
-                    self._map_[index].addField(field, value)
-                    self._field_names_.add(field) # NOOP if field exists
-                except:
-                    pass
+        def __getitem__(self, key:int):
+            return getattr(self._obj_._map_[key], self._field_, key)
+        
+        def __setitem__(self, key:int, value:int):
+            if key in self._obj_._map_:
+                if self._field_ in self._obj_._map_[key]:
+                    setattr(self._obj_._map_[key], self._field_, value)
+                else: # CAUTION: subclasses of MultiFrameIndex MAY be immutable
+                    try:
+                        self._obj_._map_[key].addField(field, value)
+                        self._obj_._field_names_.add(field) # NOOP if field exists
+                    except:
+                        pass
+            else:# add a new frame
+                if not isinstance(key, int):
+                    raise TypeError(f"Expecting an int for master 'index'; got {type(key).__name__} ")
+                # CAUTION: subclasses of MultiFrameIndex MAY expect specific arguments
+                # NOTE: no checking on value's attributes here ... this MAY be done
+                # by the MultiFrameIndex initializer (self._index_type_ SHOULD inherit
+                # from this)
+                init_sig = classify_signature(self._obj_._index_type_.__init__)
+                pos_named = init_sig.positional | init_sig.named
                 
-        else: # add a new frame
-            if not isinstance(index, int):
-                raise TypeError(f"Expectinf an int for master 'index'; got {type(index).__name__} ")
-            # CAUTION: subclasses of MultiFrameIndex MAY expect specific arguments
-            # NOTE: no checking on value's attributes here ... this MAY be done
-            # by the MultiFrameIndex initializer (self._index_type_ SHOULD inherit
-            # from this)
-            init_sig = classify_signature(self._index_type_.__init__)
-            pos_named = init_sig.positional | init_sig.named
-            
-            kwargs = dict()
-            
-            if len(init_sig.varkw) == 0:
-                if field not in pos_named:
-                    raise ArgumentError(f"{field} is an unexpected argument for {self._index_type_.__name__}")
+                kwargs = dict()
                 
-            elif field in init_sig.kwargs:
-                kwargs[field] = value
-                
-            
-            if field in init_sig.positional:
-                pos_only = tuple(value if k == field else None for k in init_sig.positional if k != "self")
-                
-                if field in init_sig.named:
-                    raise ArgumentError(f"duplicate specification for {field}")
-                
-            else:
-                pos_only = tuple()
-                
-            if field in init_sig.named:
-                # add it to kwargs!
-                if field not in kwargs:
+                if len(init_sig.varkw) == 0:
+                    if field not in pos_named:
+                        raise ArgumentError(f"{field} is an unexpected argument for {self._index_type_.__name__}")
+                    
+                elif field in init_sig.kwargs:
                     kwargs[field] = value
-                else:
-                    raise ArgumentError(f"duplicate specification for {field}")
                 
-            self._map_[index] = self._index_type_(*pos_only, **kwargs)
-            self._field_names_.add(field) # NOOP if field exists
-            
-            #try:
-            
-            #except:
-                #pass # fails silently
-    
-    @classmethod
-    def __bind__(cls, func, instance, as_name=None):
-        """
-        See https://stackoverflow.com/questions/1015307/python-bind-an-unbound-method/1015405#1015405
+                if field in init_sig.positional:
+                    pos_only = tuple(value if k == field else None for k in init_sig.positional if k != "self")
+                    
+                    if field in init_sig.named:
+                        raise ArgumentError(f"duplicate specification for {field}")
+                    
+                else:
+                    pos_only = tuple()
+                    
+                if field in init_sig.named:
+                    # add it to kwargs!
+                    if field not in kwargs:
+                        kwargs[field] = value
+                    else:
+                        raise ArgumentError(f"duplicate specification for {field}")
+                    
+                self._obj_._map_[key] = self._obj_._index_type_(*pos_only, **kwargs)
+                self._obj_._field_names_.add(field) # NOOP if field exists
         
-        """
-            
-    def __init__(self, data=None, *args):
+    
+    def __init__(self, data:typing.Optional[BaseScipyenData]=None, *args):
         """
         data: python object where data is stored framewise in a '_data_children_'
         :class: attribute; optional, default is None
@@ -206,7 +216,15 @@ class FrameIndexLookup(object):
                 fields: 'scene', 'scans', 'electrophysiology', mapped to either
                 an int value, or to None.
         """
-        data_children = getattr(data, "_data_children_", None)
+        if len(args) == 0:
+            if isinstance(data, dict) and all(isinstance(k, int) and isinstance(v, (type(None), MultiFrameIndex)) for k,v in data.items()):
+                args = (data,)
+                data = None
+                
+            elif not isinstance(data, (type(None), BaseScipyenData)):
+                raise TypeError(f"'data expected to be a BaseScipyenData object or None; got {type(data).__name__} instead")
+            
+        data_children = getattr(data, "_data_children_", tuple())
         field_names = set(c[0] for c in data_children if isinstance(c[0], str) and len(c[0].strip()))
         
         self._map_ = dict()
@@ -216,6 +234,7 @@ class FrameIndexLookup(object):
         for arg in args:
             if not isinstance(arg, dict) or len(arg) == 0 or not all(isinstance(k, int) for k in arg) or not all(isinstance(v, (MultiFrameIndex, type(None))) for v in arg.values()):
                 raise TypeError("Expecting a mapping of int keys to MultiFrameIndex objects or to None")
+            
             # NOTE: 2021-12-01 15:38:06
             # Loosely check for field names consistency:
             #
@@ -226,7 +245,7 @@ class FrameIndexLookup(object):
             # MultiFrameIndex objects (as they might in principle NOT supply a frame
             # index for all relevant fields)
             
-            for v in arg.values():
+            for k,v in enumerate(arg.values()):
                 if v is not None:
                     # make sure that all multi frame index objects are instances of the same type
                     if self._index_type_ is None:
@@ -239,7 +258,7 @@ class FrameIndexLookup(object):
                     # either the same, or at least have fields in common
                     v_fields = set(v.fieldNames())
                     
-                    if len(field_names):
+                    if len(field_names) == 0:
                         field_names = v_fields
                     else:
                         if field_names.isdisjoint(v_fields):
@@ -249,12 +268,11 @@ class FrameIndexLookup(object):
                         
             self._map_.update(arg)
             
-            
         self._field_names_ = field_names # for reference
         
         for field in self._field_names_:
-            pass
-        
+            setattr(type(self), field, self.__IndexProxy__(field))
+             
     @property
     def map(self):
         return self._map_
@@ -285,40 +303,40 @@ class FrameIndexLookup(object):
         
     
         
-    def scene(self, frame:int):
-        if frame in self._map_:
-            return self._map_[frame].scene if isinstance(self._map_, MultiFrameIndex) else None
+    #def scene(self, frame:int):
+        #if frame in self._map_:
+            #return self._map_[frame].scene if isinstance(self._map_, MultiFrameIndex) else None
         
-    def setScene(self, frame:int, value:typing.Optional[int] = None):
-        frameindex = self._map_.get(frame, None)
-        if isinstance(frameindex, MultiFrameIndex):
-            frameindex.scene = value
+    #def setScene(self, frame:int, value:typing.Optional[int] = None):
+        #frameindex = self._map_.get(frame, None)
+        #if isinstance(frameindex, MultiFrameIndex):
+            #frameindex.scene = value
             
-        elif isinstance(value, int):
-            # create frame index of it doesn't exist and value is an int
-            self._map_[frame] = MultiFrameIndex(scene=value)
+        #elif isinstance(value, int):
+            ## create frame index of it doesn't exist and value is an int
+            #self._map_[frame] = MultiFrameIndex(scene=value)
             
-    def scans(self, frame:int):
-        if frame in self._map_:
-            return self._map_[frame].scans if isinstance(self._map_, MultiFrameIndex) else None
+    #def scans(self, frame:int):
+        #if frame in self._map_:
+            #return self._map_[frame].scans if isinstance(self._map_, MultiFrameIndex) else None
         
-    def setScans(self, frame:int, value:typing.Optional[int]):
-        frameindex = self._map_.get(frame, None)
-        if isinstance(frameindex, MultiFrameIndex):
-            frameindex.scene = value
+    #def setScans(self, frame:int, value:typing.Optional[int]):
+        #frameindex = self._map_.get(frame, None)
+        #if isinstance(frameindex, MultiFrameIndex):
+            #frameindex.scene = value
             
-        elif isinstance(value, int):
-            self._map_[frame] = MultiFrameIndex(scans=value)
+        #elif isinstance(value, int):
+            #self._map_[frame] = MultiFrameIndex(scans=value)
         
-    def electrophysiology(self, frame:int):
-        if frame in self._map_:
-            return self._map_[frame].electrophysiology if isinstance(self._map_, MultiFrameIndex) else None
+    #def electrophysiology(self, frame:int):
+        #if frame in self._map_:
+            #return self._map_[frame].electrophysiology if isinstance(self._map_, MultiFrameIndex) else None
         
-    def setElectrophysiology(self, frame:int, value:typing.Optional[int]):
-        frameindex = self._map_.get(frame, None)
-        if isinstance(frameindex, MultiFrameIndex):
-            frameindex.electrophysiology = value
+    #def setElectrophysiology(self, frame:int, value:typing.Optional[int]):
+        #frameindex = self._map_.get(frame, None)
+        #if isinstance(frameindex, MultiFrameIndex):
+            #frameindex.electrophysiology = value
             
-        elif isinstance(value, int):
-            self._map_[frame] = MultiFrameIndex(electrophysiology = value)
+        #elif isinstance(value, int):
+            #self._map_[frame] = MultiFrameIndex(electrophysiology = value)
         
