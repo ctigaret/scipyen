7d7d9e4c556a5ea426226c396911139bb8645f6d
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2021-12-21 12:03:02 +0000

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- iolib/jsonio.py
+++ iolib/jsonio.py
@@ -2,6 +2,22 @@
 """
 
 import json, sys, traceback, typing, collections, inspect
+
+#### BEGIN HACK workaround named tuples
+try:
+    from _json import encode_basestring_ascii as c_encode_basestring_ascii
+except ImportError:
+    c_encode_basestring_ascii = None
+try:
+    from _json import encode_basestring as c_encode_basestring
+except ImportError:
+    c_encode_basestring = None
+try:
+    from _json import make_encoder as c_make_encoder
+except ImportError:
+    c_make_encoder = None
+#### END HACK workaround named tuples
+
 from collections import deque, namedtuple
 from functools import (singledispatch, singledispatchmethod, 
                        update_wrapper, wraps,)
@@ -14,6 +30,18 @@ from traitlets.utils.importstring import import_item
 from core import quantities as cq
 from core import prog
 from core.prog import classifySignature, resolveObject #, typedDispatch
+
+class NamedTupleWrapper(object):
+    """Wraps a named tuple so that it can be properly encoded/decoded in JSON
+    """
+    def __init__(self, obj):
+        from core.datatypes import is_namedtuple
+        if not is_namedtuple(obj):
+            raise TypeError(f"Expecting a named tuple; got {type(obj).__name__} instead")
+        
+        self.obj = obj
+                
+        
 
 class CustomEncoder(json.JSONEncoder):
     """Almost complete round trip for a subset of Python types - encoding side.
@@ -294,8 +322,13 @@ class CustomEncoder(json.JSONEncoder):
         """Almost general case
         
         """
+        from core.datatypes import is_namedtuple
         hdr, ret = self.makeJSONStub(o)
-        ret.update({"__value__": json.JSONEncoder.default(self, o)})
+        if is_namedtuple(o):
+            ret.update({"__named__": dict((f, getattr(o.obj,f)) for f in o.obj._fields),
+                "__subtype__": "collections.namedtuple"})
+        else:
+            ret.update({"__value__": json.JSONEncoder.default(self, o)})
         return {hdr:ret}
     
     @object2JSON.register(type)
@@ -303,14 +336,16 @@ class CustomEncoder(json.JSONEncoder):
         hdr, ret = self.makeJSONStub(o)
         return {hdr:ret}
     
-    @object2JSON.register(tuple)
-    def _(self, o:tuple):
-        from core.datatypes import is_namedtuple
-        if is_namedtuple(o):
-            return {"__named__": dict((f, getattr(o,f)) for f in o._fields),
-                    "__subtype__": "collections.namedtuple"}
+    @object2JSON.register(NamedTupleWrapper)
+    def _(self, o:NamedTupleWrapper):
+        # NOTE: 2021-12-21 10:33:42
+        # namedtuples need to be wrapped like this in order to force json to
+        # call this encoder's default (the 'culprit' is json._make_iterencode, 
+        # which encodes the types supported by JSON directly, BEFORE ever calling
+        # encoder.default)
         
-        return {"__value__": json.JSONEncoder.default(self, o)}
+        return {"__named__": dict((f, getattr(o.obj,f)) for f in o.obj._fields),
+                "__subtype__": "collections.namedtuple"}
             
     @object2JSON.register(complex)
     def _(self, o:complex):
@@ -323,6 +358,64 @@ class CustomEncoder(json.JSONEncoder):
         xy = kernel2array(o, True)
         return {"__args__" : xy.tolist(),
                 "__init__": classifySignature(kernelfromjson)}
+    
+    def iterencode(self, o, _one_shot=False):
+        """Overrides the json's default iterencode for namded tuples
+        FIXME: for collections, any contained namedtuple is treated as a regular
+        # tuple (see json._make_iterencode)!
+        """
+        from core.datatypes import is_namedtuple
+        print("iterencode")
+        if self.check_circular:
+            markers = {}
+        else:
+            markers = None
+        if self.ensure_ascii:
+            _encoder = encode_basestring_ascii
+        else:
+            _encoder = encode_basestring
+
+        def floatstr(o, allow_nan=self.allow_nan,
+                _repr=float.__repr__, _inf=INFINITY, _neginf=-INFINITY):
+            # Check for specials.  Note that this type of test is processor
+            # and/or platform-specific, so do tests which don't depend on the
+            # internals.
+
+            if o != o:
+                text = 'NaN'
+            elif o == _inf:
+                text = 'Infinity'
+            elif o == _neginf:
+                text = '-Infinity'
+            else:
+                return _repr(o)
+
+            if not allow_nan:
+                raise ValueError(
+                    "Out of range float values are not JSON compliant: " +
+                    repr(o))
+
+            return text
+
+        if is_namedtuple(o):
+            o = NamedTupleWrapper(o)
+
+        if (_one_shot and c_make_encoder is not None
+                and self.indent is None):
+            # NOTE: 2021-12-21 11:53:27
+            # only for one shot AND is the C encoder is available
+            _iterencode = c_make_encoder(
+                markers, self.default, _encoder, self.indent,
+                self.key_separator, self.item_separator, self.sort_keys,
+                self.skipkeys, self.allow_nan)
+        else:
+            _iterencode = _make_iterencode(
+                markers, self.default, _encoder, self.indent, floatstr,
+                self.key_separator, self.item_separator, self.sort_keys,
+                self.skipkeys, _one_shot)
+        return _iterencode(o, 0)
+            
+        #return super().iterencode(o, _one_shot=_one_shot)
         
     def default(self, obj):
         # NOTE: 2021-12-17 22:59:34
@@ -1033,7 +1126,10 @@ def decode_hook(dct):
         return dct
     
 def dumps(obj, *args, **kwargs):
+    from core.datatypes import is_namedtuple
     kwargs["cls"] = CustomEncoder
+    #if is_namedtuple(obj):
+        #obj = NamedTupleWrapper(obj)
     return json.dumps(obj, *args, **kwargs)
 
 def loads(s, *args, **kwargs):
@@ -1209,4 +1305,199 @@ def kernelfromjson(kernelcoords:list, *args, **kwargs):
     xy = np.array(kernelcoords)
     return kernelfromarray(xy)
 
+#### BEGIN HACK workaround named tuples
+def _make_iterencode(markers, _default, _encoder, _indent, _floatstr,
+        _key_separator, _item_separator, _sort_keys, _skipkeys, _one_shot,
+        ## HACK: hand-optimized bytecode; turn globals into locals
+        ValueError=ValueError,
+        dict=dict,
+        float=float,
+        id=id,
+        int=int,
+        isinstance=isinstance,
+        list=list,
+        str=str,
+        tuple=tuple,
+        _intstr=int.__repr__,
+    ):
+
+    from core.datatypes import is_namedtuple
     
+    if _indent is not None and not isinstance(_indent, str):
+        _indent = ' ' * _indent
+
+    def _iterencode_list(lst, _current_indent_level):
+        if not lst:
+            yield '[]'
+            return
+        if markers is not None:
+            markerid = id(lst)
+            if markerid in markers:
+                raise ValueError("Circular reference detected")
+            markers[markerid] = lst
+        buf = '['
+        if _indent is not None:
+            _current_indent_level += 1
+            newline_indent = '\n' + _indent * _current_indent_level
+            separator = _item_separator + newline_indent
+            buf += newline_indent
+        else:
+            newline_indent = None
+            separator = _item_separator
+        first = True
+        for value in lst:
+            if first:
+                first = False
+            else:
+                buf = separator
+            if isinstance(value, str):
+                yield buf + _encoder(value)
+            elif value is None:
+                yield buf + 'null'
+            elif value is True:
+                yield buf + 'true'
+            elif value is False:
+                yield buf + 'false'
+            elif isinstance(value, int):
+                # Subclasses of int/float may override __repr__, but we still
+                # want to encode them as integers/floats in JSON. One example
+                # within the standard library is IntEnum.
+                yield buf + _intstr(value)
+            elif isinstance(value, float):
+                # see comment above for int
+                yield buf + _floatstr(value)
+            else:
+                yield buf
+                if is_namedtuple(value):
+                    chunks = _iterencode(NamedTupleWrapper(value), _current_indent_level)
+                elif isinstance(value, (list, tuple)):
+                    chunks = _iterencode_list(value, _current_indent_level)
+                elif isinstance(value, dict):
+                    chunks = _iterencode_dict(value, _current_indent_level)
+                else:
+                    chunks = _iterencode(value, _current_indent_level)
+                yield from chunks
+        if newline_indent is not None:
+            _current_indent_level -= 1
+            yield '\n' + _indent * _current_indent_level
+        yield ']'
+        if markers is not None:
+            del markers[markerid]
+
+    def _iterencode_dict(dct, _current_indent_level):
+        if not dct:
+            yield '{}'
+            return
+        if markers is not None:
+            markerid = id(dct)
+            if markerid in markers:
+                raise ValueError("Circular reference detected")
+            markers[markerid] = dct
+        yield '{'
+        if _indent is not None:
+            _current_indent_level += 1
+            newline_indent = '\n' + _indent * _current_indent_level
+            item_separator = _item_separator + newline_indent
+            yield newline_indent
+        else:
+            newline_indent = None
+            item_separator = _item_separator
+        first = True
+        if _sort_keys:
+            items = sorted(dct.items())
+        else:
+            items = dct.items()
+        for key, value in items:
+            if isinstance(key, str):
+                pass
+            # JavaScript is weakly typed for these, so it makes sense to
+            # also allow them.  Many encoders seem to do something like this.
+            elif isinstance(key, float):
+                # see comment for int/float in _make_iterencode
+                key = _floatstr(key)
+            elif key is True:
+                key = 'true'
+            elif key is False:
+                key = 'false'
+            elif key is None:
+                key = 'null'
+            elif isinstance(key, int):
+                # see comment for int/float in _make_iterencode
+                key = _intstr(key)
+            elif _skipkeys:
+                continue
+            else:
+                raise TypeError(f'keys must be str, int, float, bool or None, '
+                                f'not {key.__class__.__name__}')
+            if first:
+                first = False
+            else:
+                yield item_separator
+            yield _encoder(key)
+            yield _key_separator
+            if isinstance(value, str):
+                yield _encoder(value)
+            elif value is None:
+                yield 'null'
+            elif value is True:
+                yield 'true'
+            elif value is False:
+                yield 'false'
+            elif isinstance(value, int):
+                # see comment for int/float in _make_iterencode
+                yield _intstr(value)
+            elif isinstance(value, float):
+                # see comment for int/float in _make_iterencode
+                yield _floatstr(value)
+            else:
+                if is_namedtuple(value):
+                    chunks = _iterencode(NamedTupleWrapper(value), _current_indent_level)
+                elif isinstance(value, (list, tuple)):
+                    chunks = _iterencode_list(value, _current_indent_level)
+                elif isinstance(value, dict):
+                    chunks = _iterencode_dict(value, _current_indent_level)
+                else:
+                    chunks = _iterencode(value, _current_indent_level)
+                yield from chunks
+        if newline_indent is not None:
+            _current_indent_level -= 1
+            yield '\n' + _indent * _current_indent_level
+        yield '}'
+        if markers is not None:
+            del markers[markerid]
+
+    def _iterencode(o, _current_indent_level):
+        if isinstance(o, str):
+            yield _encoder(o)
+        elif o is None:
+            yield 'null'
+        elif o is True:
+            yield 'true'
+        elif o is False:
+            yield 'false'
+        elif isinstance(o, int):
+            # see comment for int/float in _make_iterencode
+            yield _intstr(o)
+        elif isinstance(o, float):
+            # see comment for int/float in _make_iterencode
+            yield _floatstr(o)
+        elif is_namedtuple(o):
+            yield 
+        elif isinstance(o, (list, tuple)):
+            yield from _iterencode_list(o, _current_indent_level)
+        elif isinstance(o, dict):
+            yield from _iterencode_dict(o, _current_indent_level)
+        else:
+            if markers is not None:
+                markerid = id(o)
+                if markerid in markers:
+                    raise ValueError("Circular reference detected")
+                markers[markerid] = o
+            o = _default(o)
+            yield from _iterencode(o, _current_indent_level)
+            if markers is not None:
+                del markers[markerid]
+                
+    return _iterencode
+
+#### END HACK workaround named tuples
