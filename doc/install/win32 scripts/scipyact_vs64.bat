43b8c4d14e473ca5f7d282753805ddb439e2196f
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2022-01-23 22:56:29 +0000

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/win32 scripts/scipyact_vs64.bat
+++ doc/install/win32 scripts/scipyact_vs64.bat
@@ -4,11 +4,14 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 rem  SET "PROMPT=(x64) $P$G"
 REM EDIT BELOW TO SET WHERE scipyenv HAS BEEN CREATED
 CALL "e:\scipyenv\Scripts\activate"
-set "LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;C:\Program Files (x86)\Microsoft SDKs\MPI\Lib;%USERPROFILE%\AppData\Local\Programs\Python\Python39;%USERPROFILE%\AppData\Local\Programs\Python\Python39\Lib;%USERPROFILE%\AppData\Local\Programs\Python\Python39\libs;%LIB%"
-set "LIBPATH=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;C:\Program Files (x86)\Microsoft SDKs\MPI\Lib;%USERPROFILE%\AppData\Local\Programs\Python\Python39;%USERPROFILE%\AppData\Local\Programs\Python\Python39\Lib;%USERPROFILE%\AppData\Local\Programs\Python\Python39\libs;%LIBPATH%"
-set "INCLUDE=%VIRTUAL_ENV%\include;C:\Program Files (x86)\Microsoft SDKs\MPI\Include;%USERPROFILE%\AppData\Local\Programs\Python\Python39\include;%INCLUDE%"
-set "PATH=%VIRTUAL_ENV%\bin;%VIRTUAL_ENV%\Scripts;C:\Program Files\Microsoft MPI\Bin;%VIRTUAL_ENV%\Boost.Build\bin;%USERPROFILE%\AppData\Local\Programs\Python\Python39\DLLs;%PATH%"
-SET PYTHONSTARTUP=%VIRTUAL_ENV%\Scripts\scipyen_startup.py
+set "SDK=e:\scipyen_sdk"
+set "LIB=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib64;%VIRTUAL_ENV%\lib\site-packages\vigra;%SDK%\lib;%SDK%\lib64;%USERPROFILE%\AppData\Local\Programs\Python\Python39\libs;%LIB%"
+set "LIBPATH=%VIRTUAL_ENV%\lib;%VIRTUAL_ENV%\lib\site-packages\vigra;%VIRTUAL_ENV%\lib64;%SDK%\lib;%SDK%\lib64;%USERPROFILE%\AppData\Local\Programs\Python\Python39\libs;%LIBPATH%"
+set "INCLUDE=%VIRTUAL_ENV%\include;%SDK%\include;%USERPROFILE%\AppData\Local\Programs\Python\Python39\include;%INCLUDE%"
+set "PATH=%VIRTUAL_ENV%\bin;%VIRTUAL_ENV%\Scripts;%SDK%\bin;%USERPROFILE%\AppData\Local\Programs\Python\Python39\DLLs;%PATH%"
+rem SET PYTHONSTARTUP=%VIRTUAL_ENV%\Scripts\scipyen_startup.py
+SET "PYTHONHOME=%USERPROFILE%\AppData\Local\Programs\Python\Python39\"
+SET "PY_HOME=%USERPROFILE%\AppData\Local\Programs\Python\Python39\"
 rem  "E:\scipyenv-msys64\bin\activate"
 SET "PROMPT=(x64 scipyenv) $P$G"
 echo on
