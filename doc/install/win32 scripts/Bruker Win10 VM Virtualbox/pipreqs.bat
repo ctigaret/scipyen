a87e02acf6cbfe9cda5545116462857c896fd077
Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>
2022-01-25 18:05:47 +0000

    Signed-off-by: Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>

---
+++ doc/install/win32 scripts/Bruker Win10 VM Virtualbox/pipreqs.bat
@@ -0,0 +1,4 @@
+z:\>pip install -r pip_requirements_0_win64.txt
+z:\>pip install -r z:\scipyen\doc\install\pip_requirements_1_win64.txt
+z:\>pip install -r z:\scipyen\doc\install\pip_requirements_2.txt
+z:\>pip install -r z:\scipyen\doc\install\pip_requirements_3.txt
---
+++ doc/install/win32 scripts/Bruker Win10 VM Virtualbox/scipyen.bat
@@ -0,0 +1,14 @@
+@echo off
+rem if not defined %VIRTUAL_ENV% CALL scipyact.bat
+
+rem  set script=%~f0
+rem  echo %script%
+
+rem  for /f %%i in (powershell.exe -command "(Get-Item %script%).Target" ) do echo [%%i]
+
+rem  echo %link_target%
+rem  SET scipyendir=%~dp0
+SET scipyendir=z:\scipyen
+
+cmd /C "python %scipyendir%\scipyen.py"
+echo on
