e5e5e874b6665e0188f648e003678d719451f607
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2022-01-27 23:05:59 +0000

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/win32 scripts/obslete/Bruker Win10 VM Virtualbox/pipreqs.bat
+++
@@ -1,4 +0,0 @@
-z:\>pip install -r pip_requirements_0_win64.txt
-z:\>pip install -r z:\scipyen\doc\install\pip_requirements_1_win64.txt
-z:\>pip install -r z:\scipyen\doc\install\pip_requirements_2.txt
-z:\>pip install -r z:\scipyen\doc\install\pip_requirements_3.txt
--- doc/install/win32 scripts/obslete/HDF5-CMake/build-VS2019-x64-virtualenv.bat
+++
@@ -1,1 +0,0 @@
-ctest -S HDF5config.cmake,BUILD_GENERATOR=VS201964,INSTALLDIR=%VIRTUAL_ENV% -C RelWithDebInfo -V -O hdf5.log
