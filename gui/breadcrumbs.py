9a9b11584ac8d3c525498cfe54c781e4e56c9648
Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>
2022-04-29 17:27:24 +0100

    Signed-off-by: Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>

---
+++ gui/breadcrumbs.py
@@ -0,0 +1,5 @@
+from PyQt5 import (QtCore, QtGui, QtWidgets, QtXmlPatterns, QtXml, QtSvg,)
+from PyQt5.QtCore import (pyqtSignal, pyqtSlot, Q_ENUMS, Q_FLAGS, pyqtProperty,)
+from PyQt5.uic import loadUiType
+
+
