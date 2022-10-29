71911c0da7b6ee96cb4ef2d63b0b8e4e03818bed
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2022-10-29 23:20:08 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- gui/parameterswidgets.py
+++ gui/parameterswidgets.py
@@ -18,10 +18,21 @@ class ModelParametersWidget(QtWidgets.QWidget):
     math.inf, respectively (but they can be altered manually/programmatically by
     accessing the spin boxes via the `widgets` property)
 
-    NOTE 2: Internally, the parameters names and values are stored in a pandas
-    DataFrame which reflects the vertical or horizontal layout of the widget. 
+    NOTE 2: Internally, the parameters names and their values passed to the 
+    constructor are stored in a pandas DataFrame which reflects the vertical or 
+    horizontal layout of the widget. 
     This DataFrame is accessible via the read-only property `parameteers` of the
     widget.
+    ATTENTION: Changes in the values of the spin boxes are not reflected in
+    changes of the corresponding values in the DataFrame.
+
+    NOTE: 3: The orientation of the widget is fixed at construction; it cannot be
+    modified on a live instance of ModelParametersWidget.
+
+    NOTE 4: This widget is intended for use models that take a relatively small
+    number of numeric parameters. Although it does support an arbitrary number of
+    parameters, is can quickly become cumbersome. In such cases, the 
+    ModelParametersEditor  widget is recommended.
     
     
     """
@@ -113,11 +124,10 @@ class ModelParametersWidget(QtWidgets.QWidget):
                     self.layout().addWidget(w, ki+1, kc, QtCore.Qt.AlignLeft)
                     
         else:
-            # df = self._parameters_.T
-            for ki, i in enumerate(df.index):
+            for ki, i in enumerate(self._parameters_.index):
                 self.layout().addWidget(QtWidgets.QLabel(i, self), ki, 0, QtCore.Qt.AlignLeft)
                 for kc, c in enumerate(self._parameters_.columns):
-                    p = df.loc[i,c]
+                    p = self._parameters_.loc[i,c]
                     # print(kc, c, ki, i, p)
                     if c not in self._widgets_.keys():
                         self._widgets_[c] = dict()
@@ -142,3 +152,25 @@ class ModelParametersWidget(QtWidgets.QWidget):
     @property
     def parameters(self):
         return self._parameters_
+    
+    @pyqtSlot(float)
+    def _slot_newvalue(self, value):
+        widget = self.sender()
+        if isinstance(widget, QtWidgets.QDoubleSpinBox):
+            # index = self.layout().indexOf(widget)
+            
+            # perform reverse lookup of the widget location in self._widgets_
+            
+            for k in self._widgets_.keys():
+                if widget in self._widgets_[k].values():
+                    wkeys = [j for j in self._widgets_[k].keys() if self._widgets_[k] is widget]
+                    if len(wkeys):
+                        wkey = wkeys[0]
+                        if self._verticalLayout_:
+                            self._parameters_.loc[wkey, k] = widget.value()
+                        else:
+                            self._parameters_.loc[k, wkey] = widget.value()
+            
+            
+            
+        # pass
