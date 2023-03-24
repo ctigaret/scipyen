69783ab96cba0ec600482a97fd9b516d33f705bb
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2023-03-24 11:17:36 +0000

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -23,12 +23,17 @@ REM     is configured to show file extensions
 cd %scipyenroot%
 python -m virtualenv %scipyenvname%
 REM 5. Activate the environment (NOTE: you MUST use the full path)
-e:\scipyenv_3_10_10/Scripts/activate
+e:\scipyenv_3_10_10\Scripts\activate
 REM your prompt should now display the environment name
 REM 6. install pip requirements
 REM     NOTE: This assumes you have cloned Scipyen in the root of e: partition
 REM     meaning you now have a e:\scipyen directory tree
 python -m pip install -r e:\scipyen\doc\install\pip_requirements.txt
-
+REM 7. attempting to install binary vigra distribution
+REM 7.1. download vigra-1.11.1-win64-vc14.zip from here: https://github.com/ukoethe/vigra/releases
+REM 7.2. extract the zip archive somewhere
+REM 7.3. from the newly-created directory tree ("vigra-1.11.1") manually copy:
+REM the contents of "bin" subdirectory -> e:\scipyenv_3_10_10\bin
+REM the contents of "include" subdirectory -> e:\scipyenv_3_10_10\include
 
 REM SETLOCAL ENABLEDELAYEDEXPANSION
