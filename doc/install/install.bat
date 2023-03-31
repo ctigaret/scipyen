849575b510ff5d4544b69e02f4ef0078d2ac6d64
Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>
2023-03-31 09:54:04 +0100

    Signed-off-by: Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion enableextensions
+rem  setlocal enabledelayedexpansion enableextensions
 set mypath=%0
 set mydir=%~dp0
 set conda_reqs=%mydir%\conda_requirements_win.txt
@@ -7,10 +7,11 @@ set pip_reqs=%mydir%\pip_requirements_win.txt
 set default_env_name="e:\scipyenv_conda"
 set /P env_name=Enter the full path name of the new environment (no spaces, please, e.g. %default_env_name%):
 if [%env_name%] equ [] set env_name=%default_env_name%
+echo %env_name%
 call "conda create --prefix %env_name%"
 call "conda install --prefix %env_name% --file %conda_reqs%"
 call "conda activate %env_name%"
 call "pip install -r %pip_reqs%"
 
-:eof
-endlocal
+rem  :eof
+rem  endlocal
