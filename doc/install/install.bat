0bcbfb85392f70f7e23a3dbd9608dda0a90a2348
Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>
2023-03-31 15:26:29 +0100

    Signed-off-by: Cezar M. Tigaret <cezar@w009.psych.cf.ac.uk>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -11,37 +11,38 @@ echo %env_name%
 call conda create --prefix %env_name%
 call conda activate %env_name%
 call conda config --add channels conda-forge
-call conda install --prefix %env_name% -y --file %conda_reqs%
-rem  call conda install --previx %env_name% -y jupyter
-rem  call conda install --previx %env_name% -y jupyter_cms
-rem  call conda install --previx %env_name% -y jupyter_qtconsole_colorschemes
-rem  call conda install --previx %env_name% -y numpy
-rem  call conda install --previx %env_name% -y matplotlib
-rem  call conda install --previx %env_name% -y scipy
-rem  call conda install --previx %env_name% -y sympy
-rem  call conda install --previx %env_name% -y h5py
-rem  call conda install --previx %env_name% -y pyqtgraph
-rem  call conda install --previx %env_name% -y pandas
-rem  call conda install --previx %env_name% -y quantities
-rem  call conda install --previx %env_name% -y python-neo
-rem  call conda install --previx %env_name% -y -c conda-forge vigra
-rem  call conda install --previx %env_name% -y cmocean
-rem  call conda install --previx %env_name% -y confuse
-rem  call conda install --previx %env_name% -y inflect
-rem  call conda install --previx %env_name% -y seaborn
-rem  call conda install --previx %env_name% -y pingouin
-rem  call conda install --previx %env_name% -y qimage2ndarray
-rem  call conda install --previx %env_name% -y pyxdg
+rem  call conda install --prefix %env_name% -y --file %conda_reqs%
+call conda install --previx %env_name% -y jupyter
+call conda install --previx %env_name% -y jupyter_cms
+call conda install --previx %env_name% -y jupyter_qtconsole_colorschemes
+call conda install --previx %env_name% -y numpy
+call conda install --previx %env_name% -y matplotlib
+call conda install --previx %env_name% -y scipy
+call conda install --previx %env_name% -y sympy
+call conda install --previx %env_name% -y h5py
+call conda install --previx %env_name% -y pyqtgraph
+call conda install --previx %env_name% -y PyWavelets
+call conda install --previx %env_name% -y pandas
+call conda install --previx %env_name% -y quantities
+call conda install --previx %env_name% -y python-neo
+call conda install --previx %env_name% -y -c conda-forge vigra
+call conda install --previx %env_name% -y cmocean
+call conda install --previx %env_name% -y confuse
+call conda install --previx %env_name% -y inflect
+call conda install --previx %env_name% -y seaborn
+call conda install --previx %env_name% -y pingouin
+call conda install --previx %env_name% -y qimage2ndarray
+call conda install --previx %env_name% -y pyxdg
 REM OPTIONAL PACKAGES FROM CONDA
 REM call conda install --previx %env_name% -y qdarkstyle
-rem  call conda install --previx %env_name% -y bokeh
-rem  call conda install --previx %env_name% -y scikit-image
-rem  call conda install --previx %env_name% -y scikit-learn
-rem  call conda install --previx %env_name% -y dill
-rem  call conda install --previx %env_name% -y jupyterthemes
-rem  call conda install --previx %env_name% -y libNeuroML
-rem  call conda install --previx %env_name% -y matlab_kernel
-rem  call conda install --previx %env_name% -y octave-kernel
+call conda install --previx %env_name% -y bokeh
+call conda install --previx %env_name% -y scikit-image
+call conda install --previx %env_name% -y scikit-learn
+call conda install --previx %env_name% -y dill
+call conda install --previx %env_name% -y jupyterthemes
+call conda install --previx %env_name% -y libNeuroML
+call conda install --previx %env_name% -y matlab_kernel
+call conda install --previx %env_name% -y octave-kernel
 
 
 
