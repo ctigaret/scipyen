d36a337fc8f9f864832c7ee4ef0108eb9b398cde
Cezar Tigaret <cezar.tigaret@gmail.com>
2023-04-01 22:50:50 +0100

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -4,7 +4,7 @@ set mypath=%0
 set mydir=%~dp0
 set conda_reqs=%mydir%\conda_requirements_win.txt
 set pip_reqs=%mydir%\pip_requirements_win.txt
-set default_env_name="e:\scipyenv_conda"
+set default_env_name="e:\scipyenv"
 set /P env_name=Enter the full path name of the new environment (no spaces, please, e.g. %default_env_name%):
 if [%env_name%] equ [] set env_name=%default_env_name%
 echo %env_name%
