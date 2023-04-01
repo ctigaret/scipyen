0e3db030872443c09bc05ab2e61b8a8b4bc1e08d
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2023-04-01 13:47:15 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

---
+++ doc/install/make_link.ps1
@@ -0,0 +1,15 @@
+$strEnvPath="e:\scipyenv"
+$strScipyenPy="e:\scipyen\scipyen.py"
+
+
+$strTargetPath="%windir%\System32\cmd.exe /K C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3 && conda activate e:\scipyenv && python e:\scipyen\scipyen.py"
+$strLinkFile="$env:Public\Public Desktop\Scipyen.lnk"
+
+# param ($strEnvPath="e:\scipyenv")
+# param ($strScipyenPy="e:\scipyen\scipyen.py")
+#
+#
+# param ($strTargetPath="%windir%\System32\cmd.exe /K C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3 && conda activate e:\scipyenv && python e:\scipyen\scipyen.py")
+# param ($strLinkFile="$env:Public\Public Desktop\Scipyen.lnk")
+#
+
