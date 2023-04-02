fb3ce80129cd342eff405615bb794d5cbadf0a47
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2023-04-02 23:14:18 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -5,79 +5,80 @@ set mydir=%~dp0
 set conda_reqs=%mydir%\conda_requirements_win.txt
 set pip_reqs=%mydir%\pip_requirements_win.txt
 set default_env_name="e:\scipyenv"
-set /P env_name=Enter the full path name of the new environment (no spaces, please, e.g. %default_env_name%):
+set /P env_name="Enter the full path name of the new environment (no spaces, please, e.g. %default_env_name%): "
 if [%env_name%] equ [] set env_name=%default_env_name%
-echo "Creating conda environment " %env_name%
-call conda create --prefix %env_name%
-echo "Activating conda environment " %env_name%
-call conda activate %env_name%
-call conda config --add channels conda-forge
+echo Creating conda environment %env_name%
+call conda create -y --prefix %env_name% python=3.11 || exit
+echo Activating conda environment %env_name%
+call conda activate %env_name% || exit
+call conda config --add channels conda-forge || exit
 rem  call conda install --prefix %env_name% -y --file %conda_reqs%
-echo "Installing jupyter"
-call conda install --prefix %env_name% -y jupyter
-echo "Installing jupyter_cms"
-call conda install --prefix %env_name% -y jupyter_cms
-echo "Installing jupyter_qtconsole_colorschemes"
-call conda install --prefix %env_name% -y jupyter_qtconsole_colorschemes
-echo "Installing jupyterthemes"
-call conda install --prefix %env_name% -y jupyterthemes
-echo "Installing numpy"
-call conda install --prefix %env_name% -y numpy
-echo "Installing matplotlib"
-call conda install --prefix %env_name% -y matplotlib
-echo "Installing scipy"
-call conda install --prefix %env_name% -y scipy
-echo "Installing sympy"
-call conda install --prefix %env_name% -y sympy
-echo "Installing h5py"
-call conda install --prefix %env_name% -y h5py
-echo "Installing pyqtgraph"
-call conda install --prefix %env_name% -y pyqtgraph
-echo "Installing pywavelets"
-call conda install --prefix %env_name% -y PyWavelets
-echo "Installing pandas"
-call conda install --prefix %env_name% -y pandas
-echo "Installing quantities"
-call conda install --prefix %env_name% -y quantities
-echo "Installing python-neo"
-call conda install --prefix %env_name% -y python-neo
-echo "Installing vigra"
-call conda install --prefix %env_name% -y -c conda-forge vigra
-echo "Installing cmocean"
-call conda install --prefix %env_name% -y cmocean
-echo "Installing confuse"
-call conda install --prefix %env_name% -y confuse
-echo "Installing inflect"
-call conda install --prefix %env_name% -y inflect
-echo "Installing seaborn"
-call conda install --prefix %env_name% -y seaborn
-echo "Installing pingouin"
-call conda install --prefix %env_name% -y pingouin
-echo "Installing qimage2ndarray"
-call conda install --prefix %env_name% -y qimage2ndarray
-echo "Installing pyxdg"
-call conda install --prefix %env_name% -y pyxdg
+echo Installing jupyter
+call conda install --prefix %env_name% -y jupyter || exit
+echo Installing jupyter_cms
+call conda install --prefix %env_name% -y jupyter_cms || exit
+echo Installing color schemes for jupyter qtconsole
+call conda install --prefix %env_name% -y jupyter_qtconsole_colorschemes || exit
+echo Installing jupyter themes
+call conda install --prefix %env_name% -y jupyterthemes || exit
+echo Installing numpy
+call conda install --prefix %env_name% -y numpy || exit
+echo Installing matplotlib
+call conda install --prefix %env_name% -y matplotlib || exit
+echo Installing scipy
+call conda install --prefix %env_name% -y scipy || exit
+echo Installing sympy
+call conda install --prefix %env_name% -y sympy || exit
+echo Installing h5py
+call conda install --prefix %env_name% -y h5py || exit
+echo Installing pyqtgraph
+call conda install --prefix %env_name% -y pyqtgraph || exit
+echo Installing pywavelets
+call conda install --prefix %env_name% -y PyWavelets || exit
+echo Installing pandas
+call conda install --prefix %env_name% -y pandas || exit
+echo Installing quantities
+call conda install --prefix %env_name% -y quantities || exit
+echo Installing python-neo
+call conda install --prefix %env_name% -y python-neo || exit
+echo Installing vigra
+call conda install --prefix %env_name% -y -c conda-forge vigra || exit
+echo Installing cmocean
+call conda install --prefix %env_name% -y cmocean || exit
+echo Installing confuse
+call conda install --prefix %env_name% -y confuse || exit
+echo Installing inflect
+call conda install --prefix %env_name% -y inflect || exit
+echo Installing seaborn
+call conda install --prefix %env_name% -y seaborn || exit
+echo Installing pingouin
+call conda install --prefix %env_name% -y pingouin || exit
+echo Installing qimage2ndarray
+call conda install --prefix %env_name% -y qimage2ndarray || exit
+echo Installing pyxdg
+call conda install --prefix %env_name% -y pyxdg || exit
 REM OPTIONAL PACKAGES FROM CONDA
 REM call conda install --prefix %env_name% -y qdarkstyle
-echo "Installing bokeh"
-call conda install --prefix %env_name% -y bokeh
-echo "Installing scikit-image"
-call conda install --prefix %env_name% -y scikit-image
-echo "Installing scikit-learn"
-call conda install --prefix %env_name% -y scikit-learn
-echo "Installing dill"
-call conda install --prefix %env_name% -y dill
-echo "Installing libNeuroML"
-call conda install --prefix %env_name% -y libNeuroML
-echo "Installing matlab kernel"
-call conda install --prefix %env_name% -y matlab_kernel
-echo "Installing octave kernel"
-call conda install --prefix %env_name% -y octave-kernel
+echo Installing bokeh
+call conda install --prefix %env_name% -y bokeh || exit
+echo Installing scikit-image
+call conda install --prefix %env_name% -y scikit-image || exit
+echo Installing scikit-learn
+call conda install --prefix %env_name% -y scikit-learn || exit
+echo Installing dill
+call conda install --prefix %env_name% -y dill || exit
+echo Installing libNeuroML
+call conda install --prefix %env_name% -y libNeuroML || exit
+echo Installing matlab kernel
+call conda install --prefix %env_name% -y matlab_kernel || exit
+echo Installing octave kernel
+call conda install --prefix %env_name% -y octave-kernel || exit
 
-echo "Installing additional PyPI packages"
-call pip install -r %pip_reqs%
+echo Installing additional PyPI packages
+call pip install -r %pip_reqs% || exit
 
-powershell -ExecutionPolicy Bypass -File %mydir%\make_link.ps1 %mydir%
+powershell -ExecutionPolicy Bypass -File %mydir%\make_link.ps1 %mydir%  || exit
+echo Scipyen can now be launched from the desktop icon
 
 
 rem  :eof
