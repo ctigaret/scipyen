3f9d9a74d85d3e67aa6f83dfaff3c89e6ae5bf2b
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2023-04-02 22:46:01 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/install.bat
+++ doc/install/install.bat
@@ -7,47 +7,78 @@ set pip_reqs=%mydir%\pip_requirements_win.txt
 set default_env_name="e:\scipyenv"
 set /P env_name=Enter the full path name of the new environment (no spaces, please, e.g. %default_env_name%):
 if [%env_name%] equ [] set env_name=%default_env_name%
-echo %env_name%
+echo "Creating conda environment " %env_name%
 call conda create --prefix %env_name%
+echo "Activating conda environment " %env_name%
 call conda activate %env_name%
 call conda config --add channels conda-forge
 rem  call conda install --prefix %env_name% -y --file %conda_reqs%
+echo "Installing jupyter"
 call conda install --prefix %env_name% -y jupyter
+echo "Installing jupyter_cms"
 call conda install --prefix %env_name% -y jupyter_cms
+echo "Installing jupyter_qtconsole_colorschemes"
 call conda install --prefix %env_name% -y jupyter_qtconsole_colorschemes
+echo "Installing jupyterthemes"
+call conda install --prefix %env_name% -y jupyterthemes
+echo "Installing numpy"
 call conda install --prefix %env_name% -y numpy
+echo "Installing matplotlib"
 call conda install --prefix %env_name% -y matplotlib
+echo "Installing scipy"
 call conda install --prefix %env_name% -y scipy
+echo "Installing sympy"
 call conda install --prefix %env_name% -y sympy
+echo "Installing h5py"
 call conda install --prefix %env_name% -y h5py
+echo "Installing pyqtgraph"
 call conda install --prefix %env_name% -y pyqtgraph
+echo "Installing pywavelets"
 call conda install --prefix %env_name% -y PyWavelets
+echo "Installing pandas"
 call conda install --prefix %env_name% -y pandas
+echo "Installing quantities"
 call conda install --prefix %env_name% -y quantities
+echo "Installing python-neo"
 call conda install --prefix %env_name% -y python-neo
+echo "Installing vigra"
 call conda install --prefix %env_name% -y -c conda-forge vigra
+echo "Installing cmocean"
 call conda install --prefix %env_name% -y cmocean
+echo "Installing confuse"
 call conda install --prefix %env_name% -y confuse
+echo "Installing inflect"
 call conda install --prefix %env_name% -y inflect
+echo "Installing seaborn"
 call conda install --prefix %env_name% -y seaborn
+echo "Installing pingouin"
 call conda install --prefix %env_name% -y pingouin
+echo "Installing qimage2ndarray"
 call conda install --prefix %env_name% -y qimage2ndarray
+echo "Installing pyxdg"
 call conda install --prefix %env_name% -y pyxdg
 REM OPTIONAL PACKAGES FROM CONDA
 REM call conda install --prefix %env_name% -y qdarkstyle
+echo "Installing bokeh"
 call conda install --prefix %env_name% -y bokeh
+echo "Installing scikit-image"
 call conda install --prefix %env_name% -y scikit-image
+echo "Installing scikit-learn"
 call conda install --prefix %env_name% -y scikit-learn
+echo "Installing dill"
 call conda install --prefix %env_name% -y dill
-call conda install --prefix %env_name% -y jupyterthemes
+echo "Installing libNeuroML"
 call conda install --prefix %env_name% -y libNeuroML
+echo "Installing matlab kernel"
 call conda install --prefix %env_name% -y matlab_kernel
+echo "Installing octave kernel"
 call conda install --prefix %env_name% -y octave-kernel
 
-
-
-
+echo "Installing additional PyPI packages"
 call pip install -r %pip_reqs%
+
+powershell -ExecutionPolicy Bypass -File %mydir%\make_link.ps1 %mydir%
+
 
 rem  :eof
 rem  endlocal
--- doc/install/make_link.ps1
+++ doc/install/make_link.ps1
@@ -1,15 +1,72 @@
-$strEnvPath="e:\scipyenv"
-$strScipyenPy="e:\scipyen\scipyen.py"
+# echo $myInvocation.MyCommand.Definition
+# echo $myInvocation.MyCommand.Path
+# see https://learn.microsoft.com/en-us/archive/blogs/virtual_pc_guy/a-self-elevating-powershell-script
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
 
-$strTargetPath="%windir%\System32\cmd.exe /K C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3 && conda activate e:\scipyenv && python e:\scipyen\scipyen.py"
-$strLinkFile="$env:Public\Public Desktop\Scipyen.lnk"
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    $desktop=$shell.SpecialFolders.Item("AllUsersDesktop")
+    # We are running "as Administrator" - so change the title and background color to indicate this
+#     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+#     $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+#     clear-host
+}
+else
+{
+    $desktop=$shell.SpecialFolders.Item("Desktop")
+    # We are not running "as Administrator" - so relaunch as administrator
 
-# param ($strEnvPath="e:\scipyenv")
-# param ($strScipyenPy="e:\scipyen\scipyen.py")
-#
-#
-# param ($strTargetPath="%windir%\System32\cmd.exe /K C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3 && conda activate e:\scipyenv && python e:\scipyen\scipyen.py")
-# param ($strLinkFile="$env:Public\Public Desktop\Scipyen.lnk")
-#
+    # Create a new process object that starts PowerShell
+#     $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
 
+    # Specify the current script path and name as a parameter
+#     $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+#     $newProcess.Arguments = $myInvocation.MyCommand.Path;
+#     $newProcess.Arguments = "-NoExit" + "&'" + $script:MyInvocation.MyCommand.Path + "'";
+
+    # Indicate that the process should be elevated
+#     $newProcess.Verb = "runas";
+
+    # Start the new process
+#     [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+#     exit
+}
+# # Run your code that needs to be elevated here
+Param ($srcdir = $(throw " Source directory required "))
+$myDrive=Split-Path -Path $MyInvocation.InvocationName -Qualifier
+$myRepo=Join-Path -Path $myDrive -ChildPath "scipyen"
+$myScipyenLaunchScript=Join-Path -Path $myRepo -ChildPath "scipyen.py"
+$myCondaEnv=$Env:CONDA_PREFIX
+$myAnaconda=$Env:CONDA_PREFIX_1
+$myActivate=Join-Path -Path $myAnaconda -ChildPath "Scripts\activate.bat"
+$targetPath="cmd.exe"
+$args = "/K $myActivate $myAnaconda && conda activate $myCondaEnv && python -Xfrozen_modules=off $myScipyenLaunchScript"
+$wdir=Join-Path -Path $Home -ChildPath "Documents"
+$shell = New-Object -ComObject WScript.Shell
+# $desktop=$shell.SpecialFolders.Item("AllUsersDesktop")
+$linkPath=Join-Path -Path $desktop -ChildPath "Scipyen.lnk"
+$iconPath=Join-Path -Path $srcdir -ChildPath "pythonbackend.ico"
+# Create desktop shortcut
+$shortcut=$shell.CreateShortcut($linkPath)
+# $shortcut | Get-Member
+$shortcut.TargetPath=$targetPath
+$shortcut.Arguments=$args
+$shortcut.IconLocation=$iconPath
+$shortcut.Workingdirectory=$wdir
+$shortcut.Save()
+# create start menu shortcut - doesn't work ?!?'
+$startPath = Join-Path -Path $shell.SpecialFolders.Item("Startup") -ChildPath "Scipyen.lnk"
+$startshortcut=$shell.CreateShortcut($startPath)
+$startshortcut.TargetPath=$targetPath
+$startshortcut.Arguments=$args
+$startshortcut.IconLocation=$iconPath
+$startshortcut.Workingdirectory=$wdir
+$startshortcut.Save()
