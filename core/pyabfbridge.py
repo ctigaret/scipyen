b64f5d379835b5db37b4434c17d65fde6b779995
Cezar Tigaret <cezar.tigaret@gmail.com>
2023-06-23 23:09:36 +0100

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- core/pyabfbridge.py
+++ core/pyabfbridge.py
@@ -26,14 +26,135 @@ import neo
 from core import quantities as scq
 from iolib.pictio import getABF
 
-try:
-    import pyabf
-    hasPyABF = True
-except:
-    hasPyABF = False
+# try:
+#     import pyabf
+#     hasPyABF = True
+# except:
+#     hasPyABF = False
+#     raise RuntimeError("This module requires the pyabf packages")
 
-# import pyabf
+import pyabf
+from pyabf.abf2.section import Section
 
+class EpochSection(Section):
+    """
+    This section contains the digital output signals for each epoch. This
+    section has been overlooked by some previous open-source ABF-reading
+    projects. Note that the digital output is a single byte, but represents
+    8 bits corresponding to 8 outputs (7->0). When working with these bits,
+    I convert it to a string like "10011101" for easy eyeballing.
+
+Cezar Tigaret <cezar.tigaret@gmail.com>
+
+The original pyabf code onlt takes into account "regular" digital bit patterns
+(i.e. 0 and 1) and overlooks the fact that Clampex allows one to specify a
+train of digital outputs PER Epoch PER output channel (Channel#0, #1 etc)
+also using a star ('*') notation, e.g.:
+
+Digital out #3-0: 00*0 
+
+etc...
+
+The difference between a "regular" digital bit flag (e.g. 0010) and a 'starred'
+one is that the 'regular' one generates a digital signal (TTL) lasting as 
+long as the "First duration" parameter, whereas the "starred" one generates 
+a TRAIN of TTLs given the specified train frequency AND impulse 
+width, all taking place within the same First duration:
+
+For example given a protocol with only one output channel (Channel #0) for 
+DAC waveform:
+
+Analog Output #0
+Waveform:
+EPOCH                    A      B      C      D      E      F      G      H      I      J
+Type                     Train  Off    Off    Off    Off    Off    Off    Off    Off    Off
+Sample rate              Fast   Fast   Fast   Fast   Fast   Fast   Fast   Fast   Fast   Fast
+First level (mV)         0      0      0      0      0      0      0      0      0      0
+Delta level (mV)         0      0      0      0      0      0      0      0      0      0
+First duration (samples) 200    0      0      0      0      0      0      0      0      0
+Delta duration (samples) 0      0      0      0      0      0      0      0      0      0
+First duration (ms)      20.0   0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0
+Delta duration (ms)      0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0
+Digital pattern #3-0     000*   0000   0000   0000   0000   0000   0000   0000   0000   0000
+Digital pattern #7-4     0000   0000   0000   0000   0000   0000   0000   0000   0000   0000
+Train Period (samples)   100    0      0      0      0      0      0      0      0      0
+Pulse Width (samples)    10     0      0      0      0      0      0      0      0      0
+Train Rate (Hz)          100.00 0.00   0.00   0.00   0.00   0.00   0.00   0.00   0.00   0.00
+Pulse Width (ms)         1.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0
+Intersweep holding: same as for signal Cmd 0.
+Digital train active logic: 1.
+
+will generate a train of 1 ms TTLs at 100 Hz covering 20 ms (hence 2 pulses, 10
+ms interval) on D0.
+
+on the other hand:
+
+Digital pattern #3-0     0001   0000   0000   0000   0000   0000   0000   0000   0000   0000
+
+will generate one TTL boxcar lasting 20 ms on D0
+    
+Moreover, both the 'regular' and the 'starred' digital outputs are subject to
+"Alternate  digital outputs" flag in the Waveform tab of the protocol editor.
+
+This means that digital bit patterns in Channel#0 and Channel#1 can be different 
+(NOTE: this only applies to Channel #0 and #1; Channel #2 and higher will
+take the same pattern as Channel#1)
+This allows alternate (i.e. interleaved) application of distinct patterns of
+TTL during a run, provided there are at least two sweeps.
+
+(When this option if unchecked, the digital bit pattern for Channel#1 is disabled)
+
+Also, NOTE that in reality the "principal" channel is the one where the "Digital
+outputs" checkbox is checked
+
+All these are stored in the ABF v2 file as follows:
+
+(Dec offset: 4096)
+
+bytes 0, 1 ⇒ Epoch num (read by pyabf)
+bytes 2, 3 ⇒ 'regular' bit pattern (read by pyabf)
+bytes 4, 5 ⇒ 'starred' bit pattern Channel#0 (NOT read by pyabf)
+bytes 6, 7 ⇒ 'regular' bit pattern Channel#1 (alternate) (NOT read by pyabf)
+bytes 8, 9 ⇒ 'starred' bit pattern Channel#1 (alternate) (NOT read by pyabf)
+
+
+    """
+
+    def __init__(self, fb):
+        Section.__init__(self, fb, 124)
+
+        self.nEpochNum = [None]*self._entryCount
+        self.nEpochDigitalOutput = [None]*self._entryCount
+        self.nEpochDigitalStarOutput = [None]*self._entryCount
+        self.nEpochDigitalOutputAlternate = [None]*self._entryCount
+        self.nEpochDigitalStarOutputAlternate = [None]*self._entryCount
+
+        # NOTE: 2023-06-23 22:36:26
+        # _entryCount is the number of configured Clampex epochs
+        # e.g. if only Epoch "A" is NOT "Off" the _entryCount is 1, etc.
+        for i in range(self._entryCount):
+            # for Epoch the _byteStart is at 4096 (decimal offset)
+            self.seek(self._byteStart + i*self._entrySize)
+            self.nEpochNum[i] = self.readInt16()
+            # self.nEpochDigitalOutput[i] = self.readInt16()
+            # NOTE: abfReader.readInt16 reads TWO bytes
+            # it looks like in Clampex >= 10.1 the digital bit pattern
+            # is not what pyabf expects
+            self.nEpochDigitalOutput[i] = self.readInt16_verbose()
+            self.nEpochDigitalStarOutput[i] = self.readInt16_verbose()
+            self.nEpochDigitalOutputAlternate[i] = self.readInt16_verbose()
+            self.nEpochDigitalStarOutputAlternate[i] = self.readInt16_verbose()
+            
+        print(f"EpochSection: nEpochDigitalOutput = {self.nEpochDigitalOutput}")
+        print(f"EpochSection: nEpochDigitalStarOutput = {self.nEpochDigitalStarOutput}")
+        print(f"EpochSection: nEpochDigitalOutputAlternate = {self.nEpochDigitalOutputAlternate}")
+        print(f"EpochSection: nEpochDigitalStarOutputAlternate = {self.nEpochDigitalStarOutputAlternate}")
+            
+            # self.nEpochNum[i] = self.readInt32()
+            # self.nEpochDigitalOutput[i] = self.readInt32()
+            
+# pyabf.epochSection.EpochSection = EpochSection
+            
 def getABFProtocolEpochs(obj, sweep:int):
     if not hasPyABF:
         warning.warn("getABF requires pyabf package")
