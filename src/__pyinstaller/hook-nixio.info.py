7ac807268bdcd1d25786687b7bd8ee2027a9b73e
Cezar Tigaret <cezar.tigaret@gmail.com>
2023-06-26 23:31:35 +0100

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- src/__pyinstaller/hook-nixio.info.py
+++ src/__pyinstaller/hook-nixio.info.py
@@ -2,3 +2,5 @@ from PyInstaller.utils.hooks import collect_data_files
 
 datas = collect_data_files("nixio.info")
 
+
+
--- src/__pyinstaller/hook-nixio.py
+++ src/__pyinstaller/hook-nixio.py
@@ -3,3 +3,5 @@ from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 datas = collect_data_files("nixio")
 hiddenimports = collect_submodules("nixio")
 
+
+
