289cb5ae6cd670cdeeb0798d261d251e9795509d
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2024-02-24 13:31:23 +0000

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- doc/install/make_link.ps1
+++ doc/install/make_link.ps1
@@ -14,36 +14,13 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 {
     $desktop=$shell.SpecialFolders.Item("AllUsersDesktop")
     $wdir=$shell.SpecialFolders.Item("AllUsersDesktop")
-    # We are running "as Administrator" - so change the title and background color to indicate this
-#     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-#     $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-#     clear-host
 }
 else
 {
     $desktop=$shell.SpecialFolders.Item("Desktop")
     $wdir=Join-Path -Path $Home -ChildPath "Documents"
-    # We are not running "as Administrator" - so relaunch as administrator
-
-    # Create a new process object that starts PowerShell
-#     $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-    # Specify the current script path and name as a parameter
-#     $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-#     $newProcess.Arguments = $myInvocation.MyCommand.Path;
-#     $newProcess.Arguments = "-NoExit" + "&'" + $script:MyInvocation.MyCommand.Path + "'";
-
-    # Indicate that the process should be elevated
-#     $newProcess.Verb = "runas";
-
-    # Start the new process
-#     [System.Diagnostics.Process]::Start($newProcess);
-
-    # Exit from the current, unelevated, process
-#     exit
 }
 # # Run your code that needs to be elevated here
-# Param ($srcdir = $(throw " Source directory required "))
 $srcdir=Split-Path -Path $MyInvocation.InvocationName -Parent
 $myDrive=Split-Path -Path $MyInvocation.InvocationName -Qualifier
 # find out where is this repository located
@@ -61,8 +38,6 @@ $myAnaconda=$Env:CONDA_PREFIX_1
 $myActivate=Join-Path -Path $myAnaconda -ChildPath "Scripts\activate.bat"
 $targetPath="cmd.exe"
 $args = "/K $myActivate $myAnaconda && conda activate $myCondaEnv && python -Xfrozen_modules=off $myScipyenLaunchScript"
-# $wdir=Join-Path -Path $Home -ChildPath "Documents"
-# $desktop=$shell.SpecialFolders.Item("AllUsersDesktop")
 $linkPath=Join-Path -Path $desktop -ChildPath "Scipyen (git).lnk"
 $iconPath=Join-Path -Path $srcdir -ChildPath "pythonbackend.ico"
 # Create desktop shortcut
