c809f73391f13a4af35bdcf52dd81cb3afed6210
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2024-03-04 22:00:21 +0000

    Add automatic installation of scipyen activation and launch scripts for Windows 10 (for running scipyen off git repository)
    WARNING: for now all is in the 'ltp' branch
    
    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

---
+++ doc/install/make_scipyen_batch_scripts.ps1
@@ -0,0 +1,71 @@
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+# if ($myWindowsPrincipal.IsInRole($adminRole))
+# {
+#     $desktop=$shell.SpecialFolders.Item("AllUsersDesktop")
+#     $wdir=$shell.SpecialFolders.Item("AllUsersDesktop")
+# }
+# else
+# {
+#     $desktop=$shell.SpecialFolders.Item("Desktop")
+#     $wdir=Join-Path -Path $Home -ChildPath "Documents"
+# }
+
+$srcdir=Split-Path -Path $MyInvocation.InvocationName -Parent
+$myDrive=Split-Path -Path $MyInvocation.InvocationName -Qualifier
+# find out where is this repository located
+$p=$srcdir
+while ( !(Test-Path -Path (Join-Path -Path $p -ChildPath ".git")))
+{
+    $p = Split-Path -Path $p
+}
+$repodir=$p
+
+$myScipyenLaunchScript=Join-Path -Path $repodir -ChildPath "src\scipyen\scipyen.py"
+$myCondaEnv=$Env:CONDA_PREFIX
+$myAnaconda=$Env:CONDA_PREFIX_1
+$myActivate=Join-Path -Path $myAnaconda -ChildPath "Scripts\activate.bat"
+$program="cmd.exe"
+$activateArgs1= "$myActivate $myAnaconda"
+$activateArgs2= "conda activate $myCondaEnv"
+
+$launchArgs="python -Xfrozen_modules=off $myScipyenLaunchScript"
+
+$activateScriptContent = @"
+@echo off
+$activateArgs1 && $activateArgs2
+"@
+
+
+$launchScriptContent = @"
+@echo off
+$activateArgs1 && $activateArgs2 && $launchArgs
+"@
+
+# Create Scripts directory in user's home
+$myScriptsDir=Join-Path -Path $HOME -ChildPath "Scripts"
+if (-Not (Test-Path -Path $myScriptsDir))
+{
+New-Item -Path $myScriptsDir -ItemType Directory
+Write-host "'$myScriptsDir' directory created"
+}
+# Check that Scripts directory is in PATH
+$hasScriptsInPath=$Env:PATH -split ";" -contains $myScriptsDir
+if (-Not $hasScriptsInPath)
+{
+$Env:PATH += ";$myScriptsDir"
+Write-host "'$myScriptsDir' was added to your PATH"
+}
+
+$activateScript=Join-Path -Path $myScriptsDir -ChildPath "scipyact.bat"
+Set-Content -Path $activateScript -Value $activateScriptContent
+Write-host "To activate scipyen's environment call $activateScript"
+$launchScript=Join-Path -Path $myScriptsDir -ChildPath "scipyen.bat"
+Set-content -Path $launchScript -Value $launchScriptContent
+Write-host "To launch scipyen call $launchScript"
+
+
