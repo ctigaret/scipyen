33ce633197c9e1f56efb395ea3b739ac75b21178
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2024-05-17 11:54:25 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

--- src/scipyen/__pyinstaller/rthooks/pyi_rth_typeguard.py
+++ src/scipyen/__pyinstaller/rthooks/pyi_rth_typeguard.py
@@ -1,24 +1,49 @@
+# def _pyi_rthook():
+#     has_typeguard_typecheck_instrument=False
+#     try:
+#         import typeguard._decorators
+#         has_typeguard_typecheck_instrument = "instrument" in typeguard._decorators.__dict__
+#
+#     except:
+#         has_typeguard_typecheck_instrument = False
+#
+#     if has_typeguard_typecheck_instrument:
+#         _old_instrument = typeguard._decorators.instrument
+#
+#         def _instrument(*args):
+#             # import sys
+#             # a0 = sys.argv.pop(0)
+#             try:
+#                 return _old_instrument(*args)
+#             finally:
+#                 return "no code associated"
+#
+#         typeguard._decorators.instrument = _instrument
+
 def _pyi_rthook():
-    has_typeguard_typecheck_instrument=False
+    """Makes typeguard._decorators.typecheced a NOOP"""
+    has_typeguard_typechecked=False
     try:
         import typeguard._decorators
-        has_typeguard_typecheck_instrument = "instrument" in typeguard._decorators.__dict__
+        has_typeguard_typechecked = "typechecked" in typeguard._decorators.__dict__
 
     except:
-        has_typeguard_typecheck_instrument = False
+        has_typeguard_typechecked = False
 
-    if has_typeguard_typecheck_instrument:
-        _old_instrument = typeguard._decorators.instrument
+    if has_typeguard_typechecked:
+        _old_typechecked = typeguard._decorators.typechecked
 
-        def _instrument(*args):
-            # import sys
-            # a0 = sys.argv.pop(0)
+        def _typechecked(*args):
+            target = args[0]
             try:
-                return _old_instrument(*args)
+                return _old_typechecked(*args)
             finally:
-                return "no code associated"
+                return target
 
-        typeguard._decorators.instrument = _instrument
+
+        typeguard._decorators.typechecked = _typechecked
+
+
 
 _pyi_rthook()
 
