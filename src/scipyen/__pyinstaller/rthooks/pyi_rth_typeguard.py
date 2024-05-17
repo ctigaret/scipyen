ceb50a9a2dc59f46b8a1196ff64488d291cef879
Cezar M. Tigaret <cezar.tigaret@gmail.com>
2024-05-17 11:14:33 +0100

    Signed-off-by: Cezar M. Tigaret <cezar.tigaret@gmail.com>

---
+++ src/scipyen/__pyinstaller/rthooks/pyi_rth_typeguard.py
@@ -0,0 +1,25 @@
+def _pyi_rthook():
+    has_typeguard_typecheck_instrument=False
+    try:
+        import typeguard._decorators
+        has_typeguard_typecheck_instrument = "instrument" in typeguard._decorators.__dict__
+
+    except:
+        has_typeguard_typecheck_instrument = False
+
+    if has_typeguard_typecheck_instrument:
+        _old_instrument = typeguard._decorators.instrument
+
+        def _instrument(*args):
+            # import sys
+            # a0 = sys.argv.pop(0)
+            try:
+                return _old_instrument(*args)
+            finally:
+                return "no code associated"
+
+        typeguard._decorators.instrument = _instrument
+
+_pyi_rthook()
+
+del _pyi_rthook
