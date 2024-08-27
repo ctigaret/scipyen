fe091aa2fe5c471e688f639c32c1e712e1336d47
ctigaret <cezar.tigaret@gmail.com>
2024-08-27 10:53:12 +0100

    Signed-off-by: ctigaret <cezar.tigaret@gmail.com>

---
+++ src/scipyen/gui/shapes.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Cezar M. Tigaret <cezar.tigaret@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+Shapely-based planar graphics 2024-08-27 10:21:39
+Work in progress
+"""
+import sys, os, re, numbers, itertools, warnings, traceback
+import typing
+import math
+from collections import (ChainMap, namedtuple, defaultdict, OrderedDict,)
+from functools import (partial, partialmethod,)
+from enum import (Enum, IntEnum,)
+from abc import (ABC, abstractmethod,)# ABCMeta)
+from copy import copy
+import numpy as np
+import scipy
+from traitlets import Bunch
+from qtpy import QtCore, QtGui, QtWidgets, QtXml
+from qtpy.QtCore import Signal, Slot, Property
+from core.datatypes import (TypeEnum, )
+from core.utilities import (reverse_mapping_lookup, reverse_dict, )
+from core.traitcontainers import DataBag
+from core.prog import (safeWrapper, deprecated,
+                       timefunc, processtimefunc,filter_type)
+#from core.utilities import (unique, index_of,)
+from core.workspacefunctions import debug_scipyen
+#### END core modules
+
+#### BEGIN other gui stuff
+from .painting_shared import (standardQtGradientTypes, standardQtGradientPresets,
+                              g2l, g2c, g2r, gradientCoordinates, 
+                              qPathElementCoordinates)
+
+import gui.scipyen_colormaps as colormaps
+from .scipyen_colormaps import ColorPalette
