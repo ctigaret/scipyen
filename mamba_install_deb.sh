bbe73616dd33b3bc6f7ed8034c4365d54c3ed78e
Cezar Tigaret <cezar.tigaret@gmail.com>
2024-09-25 23:52:05 +0100

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- mamba_install_deb.sh
+++ mamba_install_deb.sh
@@ -62,24 +62,29 @@ env_name="scipyenv"
 
 mamba create -y --name "$env_name" python=3.11 --file mamba_reqs.txt
 
-if [[ $? -ne 0 ]] ; then
+if test $? -ne 0  ; then
 echo -e "Could not create the mamba environment $env_name. Goodbye!"
 exit 1
+else
+echo -e "The mamba environment $env_name was created successfully"
 
 fi
 
 mamba activate "$env_name"
 
-if [[ $? -ne 0 ]] ; then
+if test $? -ne 0  ; then
 echo -e "Could not activate the mamba environment $env_name. Goodbye!"
 exit 1
+else
+echo -e "The mamba enviornment $env_name activated"
 fi
 
 cd "$scipyendir"/src/scipyen/gui/scipyen_console_styles
 
-if [[ $? -ne 0 ]] ; then
+if test $? -ne 0 ; then
 echo -e "Trouble looking for "$scipyendir"/src/scipyen/gui/scipyen_console_styles. Goodbye!"
 exit 1
+else echo -e "Installing PyPi packages..."
 fi
 
 pip install .
