32998a6fa41dfce708d37d94eee0f1c19a2d0f43
Cezar Tigaret <TigaretC@cardiff.ac.uk>
2024-09-26 09:33:45 +0100

    Signed-off-by: Cezar Tigaret <TigaretC@cardiff.ac.uk>

---
+++ scipyen_conda.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Cezar M. Tigaret <cezar.tigaret@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+realscript=`realpath $0`
+
+echo "$realscript"
+
+scipyendir=`dirname "$realscript"`
+
+echo "$scipyendir"
+
+mamba activate scipyenv && python -Xfrozen_modules=off "$scipyendir"/src/scipyen/scipyen.py
