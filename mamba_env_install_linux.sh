71a0940bcaaf94179afb9577fae450d7062e713a
ctigaret <cezar.tigaret@gmail.com>
2025-03-13 08:23:57 +0000

    Signed-off-by: ctigaret <cezar.tigaret@gmail.com>

--- mamba_env_install_linux.sh
+++ mamba_env_install_linux.sh
@@ -55,9 +55,11 @@ shopt -s lastpipe
 # if [[ `id -u` -eq 0 ]] ; then
 cat <<END > ${target_dir}/scipyen 
 #! /bin/sh
-if [ -z \${CONDA_PREFIX} ]; then
-conda activate && mamba activate ${scipyenvdir}
-fi
+source \${HOME}/.bashrc
+#if [ -z \${CONDA_PREFIX} ]; then
+scipyact
+#fi
+mamba activate ${scipyenvdir}
 export OUTDATED_IGNORE=1
 a=\`which xrdb\` # do we have xrdb to read the X11 resources? (on Unix almost surely yes)
 if [ \$0 == 0 ] ; then
@@ -65,7 +67,7 @@ if [ -r $scipyensrcdir/neuron_python/app-defaults/nrniv ] ; then
 xrdb -merge $scipyensrcdir/neuron_python/app-defaults/nrniv
 fi
 fi
-python -Xfrozen_modules=off ${scipyensrcdir}/scipyen.py "\$*"
+python3 --Xfrozen_modules=off ${scipyensrcdir}/scipyen.py "\$*"
 END
 shopt -u lastpipe
 chmod +x ${target_dir}/scipyen 
@@ -82,7 +84,7 @@ scipyensrcdir=${scipyendir}/src/scipyen
 scipyenvdir=${HOME}/scipyenv_mamba_3.11
 
 echo -e "Creating mamba environment in ${scipyenvdir}"
-mamba env create --prefix ${scipyenvdir} --file ${condaprojectfile}
+# mamba env create --prefix ${scipyenvdir} --file ${condaprojectfile}
 
 echo -e "\nTo install the console style KeplerDark open a new shell then call:"
 echo -e "'conda activate ${scipyenvdir}'"
