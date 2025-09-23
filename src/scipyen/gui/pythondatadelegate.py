3a28345996f0a7e321b57792de6fa9cd064a0e35
ctigaret <cezar.tigaret@gmail.com>
2025-09-23 18:35:30 +0100

    Signed-off-by: ctigaret <cezar.tigaret@gmail.com>

--- src/scipyen/gui/pythondatadelegate.py
+++ src/scipyen/gui/pythondatadelegate.py
@@ -6,12 +6,12 @@
 
 r"""
 """
-import sys, os, typing
 import qtpy
 from qtpy import (QtCore, QtGui, QtWidgets, QtXml, QtSvg, QtNetwork, )
 from qtpy.QtCore import (Signal, Slot, Property,)
 __has_PySide6__ = False
-__has_PyQt6__ =False
+__has_PyQt6__ = False
+__has_sip__ = False
 if os.environ["QT_API"] == "pyside6":
     __has_PySide6__ = True
     import PySide6
@@ -21,32 +21,57 @@ if os.environ["QT_API"] == "pyside6":
     QAction = QtGui.QAction
     QActionGroup = QtGui.QActionGroup
     QShortcut = QtGui.QShortcut
-    else:
-        if os.environ["QT_API"] == "pyqt6":
-            __has_PyQt6__ = True
-            from qtpy.uic import loadUiType
-            QAction = QtWidgets.QAction
-            QActionGroup = QtWidgets.QActionGroup
-            QShortcut = QtWidgets.QShortcut
-            
-            __has_qtdbus__ = False
-            
-            try:
-                from qtpy import QtDBus
-                __has_qtdbus__ = True
-                except:
-                    __has_qtdbus__ = False
-                    
-                    from core.prog import (safewrapper, safeguiwrapper, scipwarn, printStyled)
-                    from core.sysutils import adapt_ui_path
-                    
-                    __module_path__ = os.path.abspath(os.path.dirname(__file__))
-                    
-                    
+else:
+    if os.environ["QT_API"] == "pyqt6":
+        __has_PyQt6__ = True
+        
+    from qtpy import sip
+    from qtpy.uic import loadUiType
+    QAction = QtWidgets.QAction
+    QActionGroup = QtWidgets.QActionGroup
+    QShortcut = QtWidgets.QShortcut
+    __has_sip__ = True
+    
+__has_qtdbus__ = False
+
+try:
+    from qtpy import QtDBus
+    __has_qtdbus__ = True
+except:
+    __has_qtdbus__ = False
+
+from core.prog import (safewrapper, safeguiwrapper, scipwarn, printStyled)
+from core.sysutils import adapt_ui_path
+
+__module_path__ = os.path.abspath(os.path.dirname(__file__))
+
+import quantities as pq
+from core import scipyen_quantities as scq
+from gui.widgets import small_widgets as smw
+from gui import quickdialog as qd
+
 class PythonDataDelegate(QtWidgets.QStyleItemDelegate):
     def __init__(self, parent:typing.Optional[QtWidgets.QWidget] = None):
         super().__init__(parent=parent)
+        self._model_ = None
         
     def createEditor(self, parent:QtWidgets.QWidget, option:int, index:QtCore.QModelIndex) -> QtWidgets.QWidget | None:
-        pass
+        data = index.data(QtCore.Qt.EditRole)
+        
+        if isinstance(data, int):
+            widget = QtWidgets.QSpinBox(parent)
+        elif isinstance(data, float):
+            widget = QtWidgets.QDoubleSpinBox(parent)
+        elif isinstance(data, pq.Quantity):
+            if not isinstance(data, pq.UnitQuantity):
+                widget = smw.QuantitySpinBox(parent)
+            else:
+                widget = smw.QuantityChooserWidget(parent)
+        elif isinstance(data, str):
+            widget = QtWidgets.QLineEdit(parent)
+        else: # TODO: 2025-09-23 16:16:56 FIXME use a pushbutton to open a complex viewer/editor
+            widget = QtWidgets.QLineEdit(parent)
+        widget.setFrame(False)
+        widget.setAutoFillBackground(True)
+        return widget
         
