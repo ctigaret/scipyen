a201797b70dc69652f99bcf01c6fb87c48788c86
Cezar Tigaret <cezar.tigaret@gmail.com>
2025-11-17 22:32:17 +0000

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- src/scipyen/gui/widgets/datatreemodel.py
+++ src/scipyen/gui/widgets/datatreemodel.py
@@ -125,8 +125,10 @@ class DataTreeModel(QtCore.QAbstractItemModel):
     #
     # See interactivetreewidget.InteractiveTreeWidget.parse(…)
 
-    def __init__(self, parent:typing.Optional[QtCore.QObject]=None):
+    def __init__(self, data:typing.Any, parent:typing.Optional[QtCore.QObject]=None):
         super().__init__(parent)
+        rootDataItems = list(map(QtCore.QVariant(v), ["Object", "Type", "Value/Information"]))
+        self._rootItem_ = DataTreeItem(rootDataItems)
         
         self._editable_items_:bool = False
         
@@ -138,6 +140,8 @@ class DataTreeModel(QtCore.QAbstractItemModel):
         self._introspectPredicate_ = None
         self._showPrivateMembers_ = False
         
+        self._setupModelData(data, self._rootItem_)
+        
     def flags(self, index:QtCore.QModelIndex) -> QtCore.Qt.ItemFlag:
         flags = QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsEnabled
         if self._editable_items_:
@@ -145,13 +149,36 @@ class DataTreeModel(QtCore.QAbstractItemModel):
             
         return flags
     
+    def index(self, row:int, column:int, parent:QtCore.QModelIndex) -> QtCore.QModelIndex:
+        r"""The model index at given row & column of parent.
+    Returns an invalid model index if no such index exists in the model.
+    """
+        if not self.hasIndex(row, column, parent):
+            return QtCore.QModelIndex()
+
+        parentItem = parent.internalPointer() is parent.isValid() else self._rootItem_
+        childItem = parentItem.child(row)
+        if childItem:
+            return self.createIndex(row, column, childItem)
+        
+        return QtCore.QModelIndex()
+    
+    def parent(self, index:QtCore.QModelIndex) -> QtCore.QModelIndex:
+        # TODO 2025-11-17 22:06:48 FIXME
+        if not index.isValid():
+            return QtCore.QModelIndex()
+        childItem = index.internalPointer()
+        parentItem = childItem.parentItem()
+
+        return self.createIndex(parentItem.row(), 0, parentItem) if parentItem != self._rootItem_ else QtCore.QModelIndex()
+    
     def data(self, index:QtCore.QModelIndex, role:int = QtCore.Qt.DisplayRole) -> QtCore.QVariant:
         return QtCore.QVariant() # TODO 2025-11-16 21:02:36 FIXME
     
     def setData(self, index:QtCore.QModelIndex, value:QtCore.QVariant, 
                 role:int = QtCore.Qt.EditRole) -> bool: # TODO 2025-11-16 21:08:14 FIXME
         # TODO 2025-11-16 21:08:52 FIXME 
-        # on the enxt line, consider all roles (i.e. empty list) as the 3ʳᵈ parameter
+        # on the next line, consider all roles (i.e. empty list) as the 3ʳᵈ parameter
         self.dataChanged.emit(index, index, [role]) 
         return True
         
@@ -176,19 +203,26 @@ class DataTreeModel(QtCore.QAbstractItemModel):
         return False
         
     def rowCount(self, parent:QtCore.QModelIndex) -> int:
-        # TODO 2025-11-16 21:02:48 FIXME
-        # use a version of _dataHasChildren_ returning the number of child elements
-        return 0 
+        # data = self.data(parent, QtCore.Qt.EditRole)
+        # return self._getChildrenCount(data)
+        if parent.column() > 0:
+            return 0
+        parentItem = parent.internalPointer() if parent.isValid() else self._rootItem_
+        return parentItem.childCount()
     
     def columnCount(self, parent:QtCore.QModelIndex) -> int:
-        return 3
+        # return 3
+        if parent.isValid():
+            return parent.internalPointer().columnCount()
+        return self._rootItem_.columnCount()
+        
     
     def hasChildren(self, parent:QtCore.QModelIndex = QtCore.QModelIndex()) -> bool:
-        # FIXME 2025-11-16 22:20:20 TODO
+        return self.rowCount(parent) > 0
         # use self.rowCount(parent) > 0
-        data = self.data(parent, QtCore.Qt.EditRole)
-        
-        return self._dataHasChildren_(data)
+#         data = self.data(parent, QtCore.Qt.EditRole)
+#         
+#         return self._dataHasChildren_(data)
     
 # ### BEGIN Use the superclass defaults
 #
@@ -210,7 +244,65 @@ class DataTreeModel(QtCore.QAbstractItemModel):
 #
 # ### END   Use the superclass defaults
     
-    def _dataHasChildren_(self, o:typing.Any) -> bool:
+    def _getChildrenCount(self, data:typing.Any) -> int:
+        if isinstance(data, type):
+            return len(data.__members__) if isinstance(data, enum.EnumType) else 0
+        
+        elif isinstance(data, NestedFinder.nesting_types + (set, )):
+            return len(data)
+        
+        elif isinstance(data, types.SimpleNamespace):
+            return len(data.__dict__)
+        
+        elif isinstance(data, AxesCalibration):
+            return len(data.calibrations)
+        
+        elif isinstance(data, AxisCalibrationData):
+            return len(data.channels) if data.isChannels else 0
+        
+        elif isinstance(data, ChannelCalibrationData):
+            return 0
+        
+        elif isinstance(data, PVObject):
+            return len(data.as_dict())
+        
+        elif isinstance(data, Interval):
+            # children = {"t0": data.t0, "t1": data.t1, "durations": data.durations, 
+            #             "extent": data.extent, "labels": data.labels,
+            #             "annotations": data.annotations,
+            #             "description": data.description,
+            #             }
+            return 7
+        
+        elif isinstance(data, (neo.Epoch, DataZone)):
+            # children = {"times": data.times, "durations": data.durations, 
+            #             "labels": data.labels, "annotations": data.annotations,
+            #             "description": data.description}
+            return 5
+        
+        elif isinstance(data, (neo.Event, DataMark, TriggerEvent)):
+            # children = {"times": data.times, "labels": data.labels}
+            if isinstance(data, (DataMark, TriggerEvent)):
+                # children.update({"type": data.type, "relative": data.relative})
+                return 6
+            # children.update({"annotations": data.annotations,
+            #                     "description": data.description})
+            return 4
+        
+        elif isinstance(data, scipy.optimize.Bounds):
+            # children = {"lb": data.lb, "ub": data.ub, "keep_feasible": data.keep_feasible}
+            return 3
+            
+        elif dataclasses.is_dataclass(data):
+            return len(dataclasses.fields(data))
+        
+        else:
+            if self.introspectionEnabled:
+                return len(datatypes.inspect_members(data, self.introspectionPredicate))
+            else:
+                return False
+        
+    def _dataHasChildren_(self, data:typing.Any) -> bool:
         # FIXME 2025-11-16 22:19:49 TODO: use rowCount() on the representation QModelIndex
 #         if issubclass(type(data), PODS):
 #             return False
@@ -220,6 +312,9 @@ class DataTreeModel(QtCore.QAbstractItemModel):
         
         if isinstance(data, type):
             return isinstance(data, enum.EnumType)
+        
+        elif HAVE_METAARRAY and (hasattr(data, 'implements') and data.implements('MetaArray')):
+            return 2
         
         elif isinstance(data, NestedFinder.nesting_types + (set, )):
             return len(data) > 0
@@ -253,7 +348,10 @@ class DataTreeModel(QtCore.QAbstractItemModel):
             if self.introspectionEnabled:
                 return len(datatypes.inspect_members(data, self.introspectionPredicate)) > 0
             return False
-
+        
+    def _setupModelData(self, data:typing.Any, rootItem:DataTreeItem):
+        pass
+        
     @property
     def editableItems(self) -> bool:
         return self._editable_items_
@@ -285,6 +383,71 @@ class DataTreeModel(QtCore.QAbstractItemModel):
     @showsPrivateMembers.setter
     def showsPrivateMembers(self, val:bool):
         self._showPrivateMembers_ = val == True
+    
+class DataTreeItem:
+    r"""See Simple Tree Model example in Qt documentation.
+    A DataTreeItem contains children (DataTreeItem objects) — one per row — and
+item data (QVariant objects wrapping Python data), one per column.
+    The hierarchy is established by DataTreeItem objects being children of a higher
+level DataTreeItem; at the top of the hierarchy sits the "root" item.
+"""
+    def __init__(self, data:typing.List[QtCore.QVariant], parentItem:typing.Optional[typing.Self] = None):
+        r"""DataTreeItem constructor
+    Parameters:
+    ===========
+    data: list of QVariant objects — the item data stored in this DataTreeItem
+    parentItem: the DataTreeItem, that has this DataTreeItem as a child
+    """
+        _childItems_:typing.List[typing.Self] = list()
+        _itemData_:typing.List[QtCore.QVariant] = data
+        _parentItem_:typing.Optional[typing.Self] = parentItem
+        
+    
+    def appendChild(self, child:typing.Self):
+        r"""Adds a DataTreeItem as a child of this object"""
+        self._childItems_.append(child)
+    
+    def child(self, row:int) -> typing.Self | None:
+        r"""Access the DataTreeItem child of this object, at index 'row'.
+        Returns None when 'row' is out of range.
+    """
+        if row in range(len(self._childItems_)):
+            return self._childItems_[row]
+    
+    def childCount(self) -> int:
+        r"""How many DataTreeItem children does this object have.
+    """
+        return len(self._childItems_)
+    
+    def columnCount(self) -> int:
+        r"""How many item data (QVariant objects) does this object have"""
+        return len(self._itemData_)
+    
+    def data(self, column:int, role:QtCore.Qt.ItemDataRole=QtCore.Qt.DisplayRole) -> QtCore.QVariant:
+        r"""Returns column data: the QVariant at index 'column'.
+        Returns a QVariant() (empty) when 'column' is out of range
+    """
+        # TODO 2025-11-17 22:28:44 FIXME
+        # set up item data (QVariant) for various item data roles!!!
+        if column in range(len(self._itemData_)):
+            return self._itemData_[column]
+        return QtCore.QVariant()
+    
+    def row(self) -> int:
+        r"""Index of this object in the list of children of its parent.
+        Returns 0 if the parent is None, or -1 if this object is NOT a child of 
+        its parent (unlikely, but there we go...)
+    """
+        if self._parentItem_ is None:
+            return 0
+        if self in self._parentItem_._childItems_:
+            return self._parentItem_._childItems_.index(self)
+        return -1
+    
+    def parentItem(self) -> typing.Self | None:
+        return self._parentItem_
+    
+    
     
 # @singledispatch # TODO 2025-11-16 21:59:00 implement this, if possible, replacing DataTreeModel._dataHasChildren_
 # def data_has_children(obj:typing.Any) -> bool:
